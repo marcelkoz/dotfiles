ca4c4d774a319c219187bae11ac1e59048549bef
marcelkoz <37003922+marcelkoz@users.noreply.github.com>
2021-09-12 21:31:39 +0100

    .sym_links.py: Improve formatting of output

--- .sym_links.py
+++ .sym_links.py
@@ -35,23 +35,23 @@ def make_symlinks(files):
 
         # ensure destination directories exist
         if pair[1] != user_home:
-            print('Ensuring {0} has a valid symlink destination...'.format(pair[1]))
+            print('  Ensuring ({0}) has a valid symlink destination...'.format(pair[1]))
             path = str(dest_path.parent.resolve())
             os.makedirs(path, exist_ok=True)
 
         # replace existing file with symlink
         new_dest_path = dest_path / file_name
         if new_dest_path.exists():
-            print('Path {0} already exists, removing file...'.format(new_dest_path))
+            print('  ({0}) already exists, removing file...'.format(new_dest_path))
             os.remove(new_dest_path)
 
         os.symlink(src_path, new_dest_path)
-        print('Created symlink ({0}) -> ({1})'.format(src_path, new_dest_path))
+        print('  Created symlink ({0}) -> ({1})'.format(src_path, new_dest_path))
 
 def main():
     print('Creating rc file symlinks...')
     make_symlinks(rc_files)
-    print('Finished creating rc file symlinks.')
+    print('Finished creating rc file symlinks.', end='\n\n')
 
     print('Creating config file symlinks...')
     make_symlinks(config_files)
