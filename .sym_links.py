8c3f09cbaeeb7549410ff043cfa9b9b0bdd1a0eb
Marcel Kozlowski <37003922+marcelkoz@users.noreply.github.com>
2022-08-23 20:18:11 +0100

    .sym_links.py: Refactor 'make_symlinks' function to be slimmer and more readable

--- .sym_links.py
+++ .sym_links.py
@@ -24,17 +24,17 @@ bin_src     = source    / '.bin'
 # rc files
 # (source, destination)
 rc_files = (
-    (f'{source}/.bashrc',  user_home),
-    (f'{source}/.zshrc',   user_home),
-    (f'{source}/.vimrc',   user_home),
-    (f'{source}/.inputrc', user_home),
+    (source / '.bashrc',  user_home),
+    (source / '.zshrc',   user_home),
+    (source / '.vimrc',   user_home),
+    (source / '.inputrc', user_home),
 )
 
 # config files
 # (source, destination)
 config_files = (
-    (f'{config_src}/kitty/kitty.conf', f'{config_dest}/kitty'),
-    (f'{source}/.sh_aliases',          user_home),
+    (config_src / 'kitty/kitty.conf', config_dest / 'kitty'),
+    (source / '.sh_aliases',          user_home),
 )
 
 # bin files
@@ -48,30 +48,37 @@ class InvalidPairError(Exception):
         super().__init__(reason)
         self.pair = pair
 
-def make_symlinks(files):
+class FilePair:
+    def __init__(self, source, destination):
+        self.source      = Path(source)
+        self.destination = Path(destination)
+
+def create_destination_parent(pair: FilePair):
+    print(indent, 'Ensuring ({0}) has a valid symlink destination...'.format(pair.destination))
+    parent = pair.destination.parent.resolve()
+    if not parent.exists():
+        os.makedirs(parent, exist_ok=True)
+
+def replace_destination_file(file_path: Path):
+    if safe_mode:
+        print(indent, '({0}) already exists, moving file...'.format(file_path))
+        os.rename(file_path, dup_dest / file_path.name)
+    else:
+        print(indent, '({0}) already exists, removing file...'.format(file_path))
+        os.remove(file_path)
+
+def create_symlinks(files):
     for pair in files:
-        src_path  = Path(pair[0])
-        dest_path = Path(pair[1])
-        file_name = src_path.name
+        pair = FilePair(*pair)
 
-        # ensure destination directories exist
-        if dest_path != user_home:
-            print(indent, 'Ensuring ({0}) has a valid symlink destination...'.format(pair[1]))
-            path = dest_path.parent.resolve()
-            os.makedirs(path, exist_ok=True)
+        create_destination_parent(pair)
 
-        # replace existing file with symlink
-        new_dest_path = dest_path / file_name
-        if new_dest_path.exists():
-            if safe_mode:
-                print(indent, '({0}) already exists, moving file...'.format(new_dest_path))
-                os.rename(new_dest_path, dup_dest / file_name)
-            else:
-                print(indent, '({0}) already exists, removing file...'.format(new_dest_path))
-                os.remove(new_dest_path)
+        destination_file = pair.destination / pair.source.name
+        if destination_file.exists():
+            replace_destination_file(destination_file)    
 
-        os.symlink(src_path, new_dest_path)
-        print(indent, 'Created symlink ({0}) -> ({1})'.format(src_path, new_dest_path))
+        os.symlink(pair.source, destination_file)
+        print(indent, 'Created symlink ({0}) -> ({1})'.format(pair.source, destination_file))
 
 def verify_symlinks(files):
     for pair in files:
@@ -88,7 +95,7 @@ def link_files(file_type, files):
     print(f'Finished verifying {file_type} file symlinks.', end='\n\n')
 
     print(f'Creating {file_type} file symlinks...')
-    make_symlinks(files)
+    create_symlinks(files)
     print(f'Finished creating {file_type} file symlinks.', end='\n\n')
 
 def main():
