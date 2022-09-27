75aea08579c436a55ebfd050fcf7e0134efdf5da
Marcel Kozlowski <37003922+marcelkoz@users.noreply.github.com>
2022-09-27 22:54:11 +0100

    .sym_links.py: Improve logging readability

--- .sym_links.py
+++ .sym_links.py
@@ -4,8 +4,9 @@
 #
 
 from pathlib import Path
-import os
+import logging as logger
 import typing
+import os
 
 class FilePair:
     def __init__(self, source, destination):
@@ -14,14 +15,22 @@ class FilePair:
 
     def verify(self):
         if not Path(repository).exists():
-            raise InvalidPairError('source location does not exist', self)
+            raise InvalidPairError('repository location does not exist', self)
 
     def __str__(self):
         return f'FilePair {{ Source: ({self.source}) Destination: ({self.destination}) }}'
 
+class InvalidPairError(Exception):
+    def __init__(self, reason, pair):
+        super().__init__(reason)
+        self.pair = pair
+
 #
 # Script Adjustments
 #
+
+# set logging configuration
+logger.basicConfig(level=logger.INFO, format='%(message)s')
 
 # safe mode
 safe_mode = True
@@ -57,19 +66,14 @@ bin_files = (
 #
 #
 
-class InvalidPairError(Exception):
-    def __init__(self, reason, pair):
-        super().__init__(reason)
-        self.pair = pair
-
 def create_destination_parent(pair: FilePair):
-    print(indent, 'Ensuring ({0}) has a valid symlink destination...'.format(pair.destination))
+    logger.debug(f'{indent}Ensuring ({pair.destination}) has a valid symlink destination...')
     parent = pair.destination.parent.resolve()
     if not parent.exists():
         os.makedirs(parent, exist_ok=True)
 
 def replace_destination_file(file_path: Path):
-    print(indent, '({0}) already exists, {1}moving file...'.format(file_path, '' if safe_mode else 're'))
+    logger.debug('{2}({0}) already exists, {1}moving file...'.format(file_path, '' if safe_mode else 're', indent))
 
     if safe_mode:
         os.rename(file_path, duplicates / file_path.name)
@@ -85,21 +89,19 @@ def create_symlinks(pairs: typing.List[FilePair]):
             replace_destination_file(destination_file)    
 
         os.symlink(pair.source, destination_file)
-        print(indent, 'Created symlink ({0}) -> ({1})'.format(pair.source, destination_file))
+        logger.info(f'{indent}Created symlink ({pair.source}) -> ({destination_file})')
 
 def verify_symlinks(pairs: typing.List[FilePair]):
     for pair in pairs:
         pair.verify()
-        print(indent, f'Verified ({pair.source}) -> ({pair.destination})')
+        logger.info(f'{indent}Verified ({pair.source}) -> ({pair.destination})')
 
 def link_files(file_type: str, pairs: typing.Tuple[FilePair]):
-    print(f'Verifying {file_type} file symlinks...')
+    logger.info(f'\nVerifying {file_type} file symlinks...')
     verify_symlinks(pairs)
-    print(f'Finished verifying {file_type} file symlinks.', end='\n\n')
 
-    print(f'Creating {file_type} file symlinks...')
+    logger.info(f'\nCreating {file_type} file symlinks...')
     create_symlinks(pairs)
-    print(f'Finished creating {file_type} file symlinks.', end='\n\n')
 
 def main():
     if safe_mode:
@@ -110,7 +112,7 @@ def main():
         link_files('config', config_files)
         link_files('bin',    bin_files)
     except InvalidPairError as err:
-        print(indent, f'Failed to verify pair, {err}: {err.pair}') 
+        logger.error(f'{indent}Failed to verify pair, {err}: {err.pair}') 
 
 if __name__ == '__main__':
     main()
